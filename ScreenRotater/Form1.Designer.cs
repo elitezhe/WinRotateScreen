c4b9dd689f404a5270ba24b31f4ff24553d3ea3e
Zhe Zhang <zhangzhe.zhe@qq.com>
2015-09-23 21:39:35 +0800

    Git starts! Finish rotate screen functions. Reference: https://msdn.microsoft.com/zh-cn/library/ms812499.aspx?f=255&MSPPError=-2147217396#mainSection

---
+++ ScreenRotater/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ScreenRotater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAntiClockwise = new System.Windows.Forms.Button();
+            this.buttonClockwise = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonAntiClockwise
+            // 
+            this.buttonAntiClockwise.Location = new System.Drawing.Point(12, 12);
+            this.buttonAntiClockwise.Name = "buttonAntiClockwise";
+            this.buttonAntiClockwise.Size = new System.Drawing.Size(75, 23);
+            this.buttonAntiClockwise.TabIndex = 0;
+            this.buttonAntiClockwise.Text = "逆时针转动";
+            this.buttonAntiClockwise.UseVisualStyleBackColor = true;
+            this.buttonAntiClockwise.Click += new System.EventHandler(this.buttonAntiClockwise_Click);
+            // 
+            // buttonClockwise
+            // 
+            this.buttonClockwise.Location = new System.Drawing.Point(122, 12);
+            this.buttonClockwise.Name = "buttonClockwise";
+            this.buttonClockwise.Size = new System.Drawing.Size(75, 23);
+            this.buttonClockwise.TabIndex = 1;
+            this.buttonClockwise.Text = "顺时针转动";
+            this.buttonClockwise.UseVisualStyleBackColor = true;
+            this.buttonClockwise.Click += new System.EventHandler(this.buttonClockwise_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 272);
+            this.Controls.Add(this.buttonClockwise);
+            this.Controls.Add(this.buttonAntiClockwise);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAntiClockwise;
+        private System.Windows.Forms.Button buttonClockwise;
+    }
+}
+
---
+++ ScreenRotater/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Runtime.InteropServices;
+using DisplaySettingsSample;
+
+namespace ScreenRotater
+{
+    public partial class Form1 : Form
+    {
+        private int[] orientationValues = new int[4]{NativeMethods.DMDO_DEFAULT,
+														NativeMethods.DMDO_90,
+														NativeMethods.DMDO_180,
+														NativeMethods.DMDO_270};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int GetSettings(ref DEVMODE dm)
+        {
+            // helper to obtain current settings
+            return GetSettings(ref dm, NativeMethods.ENUM_CURRENT_SETTINGS);
+        }
+
+        private int GetSettings(ref DEVMODE dm, int iModeNum)
+        {
+            // helper to wrap EnumDisplaySettings Win32 API
+            return NativeMethods.EnumDisplaySettings(null, iModeNum, ref dm);
+        }
+        private void ChangeSettings(DEVMODE dm)
+        {
+            // helper to wrap ChangeDisplaySettings Win32 API
+            //if (DialogResult.Yes == MessageBox.Show("Are you sure you want to change your display setings?", "", MessageBoxButtons.YesNo))
+            {
+                int iRet = NativeMethods.ChangeDisplaySettings(ref dm, 0);
+                switch (iRet)
+                {
+                    case NativeMethods.DISP_CHANGE_SUCCESSFUL:
+                        break;
+                    case NativeMethods.DISP_CHANGE_RESTART:
+                        MessageBox.Show("Please restart your system");
+                        break;
+                    case NativeMethods.DISP_CHANGE_FAILED:
+                        MessageBox.Show("ChangeDisplaySettigns API failed");
+                        break;
+                    case NativeMethods.DISP_CHANGE_BADDUALVIEW:
+                        MessageBox.Show("The settings change was unsuccessful because system is DualView capable.");
+                        break;
+                    case NativeMethods.DISP_CHANGE_BADFLAGS:
+                        MessageBox.Show("An invalid set of flags was passed in.");
+                        break;
+                    case NativeMethods.DISP_CHANGE_BADPARAM:
+                        MessageBox.Show("An invalid parameter was passed in. This can include an invalid flag or combination of flags.");
+                        break;
+                    case NativeMethods.DISP_CHANGE_NOTUPDATED:
+                        MessageBox.Show("Unable to write settings to the registry.");
+                        break;
+                    default:
+                        MessageBox.Show("Unknown return value from ChangeDisplaySettings API");
+                        break;
+                }
+            }
+        }
+        private void RotateClockwise()
+        {
+            DEVMODE dm = NativeMethods.CreateDevmode();
+            GetSettings(ref dm);
+
+            //swap height and width
+            int temp = dm.dmPelsHeight;
+            dm.dmPelsHeight = dm.dmPelsWidth;
+            dm.dmPelsWidth = temp;
+
+            // set the orientation value to what's next clockwise
+            int index = Array.IndexOf(orientationValues, (object)dm.dmDisplayOrientation);
+            int newIndex = (index == 0) ? 3 : index - 1;
+            dm.dmDisplayOrientation = orientationValues[newIndex];
+
+            // switch to new settings
+            ChangeSettings(dm);		
+            
+        }
+
+        private void RotateAntiClockwise()
+        {
+            // obtain current settings
+            DEVMODE dm = NativeMethods.CreateDevmode();
+            GetSettings(ref dm);
+
+            // swap height and width
+            int temp = dm.dmPelsHeight;
+            dm.dmPelsHeight = dm.dmPelsWidth;
+            dm.dmPelsWidth = temp;
+
+            // set the orientation value to what's next anti-clockwise
+            int index = Array.IndexOf(orientationValues, (object)dm.dmDisplayOrientation);
+            int newIndex = (index == 3) ? 0 : index + 1;
+            dm.dmDisplayOrientation = orientationValues[newIndex];
+
+            // switch to new settings
+            ChangeSettings(dm);
+        }
+
+        private void buttonClockwise_Click(object sender, EventArgs e)
+        {
+            RotateClockwise();
+        }
+
+        private void buttonAntiClockwise_Click(object sender, EventArgs e)
+        {
+            RotateAntiClockwise();
+        }
+    }
+}
